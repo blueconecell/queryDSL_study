5d31ffe032b0370b35ac838266c586258abe77b7
jykim <95961799+blueconecell@users.noreply.github.com>
2026-03-14 13:39:53 +0900

    test: 전체테스트, member 테스트

--- src/test/java/com/study/querydsl/QueryDslApplicationTests.java
+++ src/test/java/com/study/querydsl/QueryDslApplicationTests.java
@@ -1,12 +1,36 @@
 package com.study.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.study.querydsl.entity.Hello;
+import com.study.querydsl.entity.QHello;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Transactional
 class QueryDslApplicationTests {
+
+    @Autowired
+    EntityManager em;
+
     @Test
     void contextLoads() {
+        Hello hello = new Hello();
+        em.persist(hello);
+
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QHello qHello = QHello.hello;
+
+        Hello result = query
+                .selectFrom(qHello)
+                .fetchOne();
+        Assertions.assertEquals(result, hello);
+        Assertions.assertEquals(result.getId(), hello.getId());
+
     }
 }
